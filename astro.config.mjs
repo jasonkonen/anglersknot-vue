26e73e3211a76377cdf7319c24a9919964f10beb
jasonkonen <jason@grubbyhalo.com>
2025-03-13 11:59:21 -0500

    Configure server-side rendering with Node.js adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,12 @@ import vue from '@astrojs/vue';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  site: 'https://anglersknot.com',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [mdx(), sitemap(), vue()],
-});
-
-// https://astro.build/config
-export default defineConfig({
 	site: 'https://anglersknot.com',
+	output: 'server',
+	adapter: node({
+	  mode: 'standalone',
+	  host: '0.0.0.0',
+	  port: 4500
+	}),
 	integrations: [mdx(), sitemap(), vue()],
-});
-
+  });
