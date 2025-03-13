6c2bfe7b7a84648090ca70ed464457eb912b3a8f
jasonkonen <jason@grubbyhalo.com>
2025-03-13 11:38:42 -0500

    attempted deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  site: 'https://anglersknot.com',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  integrations: [mdx(), sitemap(), vue()],
+});
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://anglersknot.com',
 	integrations: [mdx(), sitemap(), vue()],
 });
+
